aa7a84330277c415f75f513ab1bffe5b5a4a0dd8
Thomas <git@thomas.klierlinge.de>
2025-10-14 16:40:36 +0200

    Take commands by serial

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,13 @@
 
 #include <array>
 
+struct Data {
+  std::array<float, 8> adcValues;
+};
+
 std::vector<String> setupErrors;
+
+void handleSerial();
 
 void setup() {
   Serial.begin(115200);
@@ -28,20 +34,75 @@ void setup() {
   }
 }
 
+bool showAdcValues = true;
+int delayMs = 100;
+
 int loopCount = 0;
 void loop() {
   loopCount++;
-  Serial.print(loopCount);
-  Serial.print(": ");
+  handleSerial();
+  Data data;
   const auto adcResults = ADC::readAll();
   if (!std::get<0>(adcResults).empty()) {
     for (const auto& err : std::get<0>(adcResults)) {
       Serial.println("ADC Error: " + err);
     }
   }
-  for(const auto value : std::get<1>(adcResults)) {
-    Serial.print(ADC::progress_bar(value, 5));
-    Serial.print(" ");
+  data.adcValues = std::get<1>(adcResults);
+
+  Serial.print(loopCount);
+  Serial.print(": ");
+  if (showAdcValues) {
+    for(const auto value : data.adcValues) {
+      Serial.print(ADC::progress_bar(value, 5));
+      Serial.print(" ");
+    }
   }
   Serial.println();
+  if (delayMs > 0)
+    delay(delayMs);
+}
+
+void handleSerial() {
+  while (Serial.available()) {
+    const auto cmd = Serial.read();
+    switch (cmd) {
+      case 'a':
+        showAdcValues = !showAdcValues;
+        break;
+      case '0':
+        delayMs = 0;
+        break;
+      case '1':
+        delayMs = 1;
+        break;
+      case '2':
+        delayMs = 10;
+        break;
+      case '3':
+        delayMs = 100;
+        break;
+      case '4':
+        delayMs = 1000;
+        break;
+      case 'e':
+        if (setupErrors.empty()) {
+          Serial.println("No setup errors.");
+          break;
+        }
+        for (const auto& err : setupErrors) {
+          Serial.println("Setup Error: " + err);
+        }
+        break;
+      case ' ':
+        for (int i = 9; i > 0; i--) {
+          Serial.print(i);
+          delay(1000);
+        }
+        break;
+      default:
+        Serial.println("Unknown command: '" + String((char)cmd) + "' (" + String((int)cmd) + ")");
+        break;
+    }
+  }
 }
