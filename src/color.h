959e535601609013a4375dbb4d18343cd17dd739
Thomas <git@thomas.klierlinge.de>
2025-10-17 19:13:24 +0200

    Add WS2812B output

---
+++ src/color.h
@@ -0,0 +1,62 @@
+#include <cstdint>
+
+struct Color {
+  constexpr Color(uint8_t r, uint8_t g, uint8_t b) : r(r), g(g), b(b)
+  { }
+  constexpr Color(uint8_t r, uint8_t g, uint8_t b, float brightness) : Color(
+      static_cast<uint8_t>(static_cast<float>(r) * brightness),
+      static_cast<uint8_t>(static_cast<float>(g) * brightness),
+      static_cast<uint8_t>(static_cast<float>(b) * brightness))
+  { }
+
+  constexpr operator uint32_t() const {
+    return (static_cast<uint32_t>(r) << 16) | (static_cast<uint32_t>(g) << 8) | static_cast<uint32_t>(b);
+  }
+
+  constexpr Color operator*(float brightness) const {
+    return Color{r, g, b, brightness};
+  }
+
+  constexpr Color operator/(float divisor) const {
+    return Color{
+      static_cast<uint8_t>(std::min(static_cast<int>(static_cast<float>(r) / divisor), 255)),
+      static_cast<uint8_t>(std::min(static_cast<int>(static_cast<float>(g) / divisor), 255)),
+      static_cast<uint8_t>(std::min(static_cast<int>(static_cast<float>(b) / divisor), 255))
+    };
+  }
+
+  constexpr Color operator+(const Color& other) const {
+    return Color{
+      static_cast<uint8_t>(std::min(static_cast<int>(r) + static_cast<int>(other.r), 255)),
+      static_cast<uint8_t>(std::min(static_cast<int>(g) + static_cast<int>(other.g), 255)),
+      static_cast<uint8_t>(std::min(static_cast<int>(b) + static_cast<int>(other.b), 255))
+    };
+  }
+
+  constexpr Color operator-(const Color& other) const {
+    return Color{
+      static_cast<uint8_t>(std::max(static_cast<int>(r) - static_cast<int>(other.r), 0)),
+      static_cast<uint8_t>(std::max(static_cast<int>(g) - static_cast<int>(other.g), 0)),
+      static_cast<uint8_t>(std::max(static_cast<int>(b) - static_cast<int>(other.b), 0))
+    };
+  }
+
+  constexpr Color operator*(const Color& other) const {
+    return Color{
+      static_cast<uint8_t>((static_cast<int>(r) * static_cast<int>(other.r)) / 255),
+      static_cast<uint8_t>((static_cast<int>(g) * static_cast<int>(other.g)) / 255),
+      static_cast<uint8_t>((static_cast<int>(b) * static_cast<int>(other.b)) / 255)
+    };
+  }
+
+  uint8_t r, g, b;
+};
+
+  constexpr Color RED = Color{255, 0, 0};
+  constexpr Color GREEN = Color{0, 255, 0};
+  constexpr Color BLUE = Color{0, 0, 255};
+  constexpr Color YELLOW = Color{255, 255, 0};
+  constexpr Color CYAN = Color{0, 255, 255};
+  constexpr Color MAGENTA = Color{255, 0, 255};
+  constexpr Color WHITE = Color{255, 255, 255};
+  constexpr Color BLACK = Color{0, 0, 0};
--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,84 @@
 #include "adc.h"
+#include "color.h"
 #include "i2c.h"
 
 #include <ADS1115.h>
 #include <Adafruit_MCP23X17.h>
+#include <Adafruit_NeoPixel.h>
 
 #include <Arduino.h>
 
 #include <array>
 
-struct Data {
-  std::array<float, 8> adcValues;
-  uint16_t pins;
-  bool pin(uint8_t pin) {
-    return (pins & (1 << pin)) == 0;
-  }
-};
-
+constexpr uint8_t BLINK_LED_PIN = 5;
+constexpr int NUM_LEDS = 100;
+constexpr uint8_t LED_PIN = 10;
 std::vector<String> setupErrors;
+Adafruit_MCP23X17 mcp;
+Adafruit_NeoPixel pixels(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void handleSerial();
 
-Adafruit_MCP23X17 mcp;
+constexpr std::array<uint8_t, 8> POTI_MAP = {4, 7, 6, 5, 1, 2, 3, 0};
+constexpr uint8_t POTI_CONFIG_BRIGHTNESS = POTI_MAP[0];
+constexpr uint8_t POTI_BRIGHTNESS = POTI_MAP[2];
+constexpr std::array<uint8_t, 5> BUTTONS_MAP = {4, 3, 2, 1, 0};
+constexpr std::array<uint8_t, 6> SWITCHES2_MAP = {15, 14, 13, 12, 11, 10};
+constexpr std::pair<uint8_t, uint8_t> SWITCHES3_MAP = {8, 9};
+constexpr std::array<uint8_t, 3> PLUGSS_MAP = {7, 6, 5};
 
-const std::array<uint8_t, 8> POTI_MAP = {4, 7, 6, 5, 1, 2, 3, 0};
-const std::array<uint8_t, 5> BUTTONS_MAP = {4, 3, 2, 1, 0};
-const std::array<uint8_t, 6> SWITCHES2_MAP = {15, 14, 13, 12, 11, 10};
-const std::pair<uint8_t, uint8_t> SWITCHES3_MAP = {8, 9};
+constexpr float MAX_BRIGHTNESS = 1.0f;
+constexpr float MIN_BRIGHTNESS = 1.f / 255.f;
+
+struct Data {
+  std::array<float, 8> adcValues;
+  uint16_t pins;
+
+  bool pin(uint8_t pinIndex) {
+    return (pins & (1 << pinIndex)) == 0;
+  }
+
+  bool button(uint8_t buttonIndex) {
+    if (buttonIndex >= BUTTONS_MAP.size())
+      return false;
+    return pin(BUTTONS_MAP[buttonIndex]);
+  }
+
+  bool switch2(uint8_t switchIndex) {
+    if (switchIndex >= SWITCHES2_MAP.size())
+      return false;
+    return pin(SWITCHES2_MAP[switchIndex]);
+  }
+
+  int8_t switch3() {
+    return (pin(SWITCHES3_MAP.first) ? 1 : 0) - (pin(SWITCHES3_MAP.second) ? 1 : 0);
+  }
+
+  bool plug(uint8_t plugIndex) {
+    if (plugIndex >= PLUGSS_MAP.size())
+      return false;
+    return pin(PLUGSS_MAP[plugIndex]);
+  }
+};
+
+bool showPotis = true;
+bool showButtons = true;
+bool showSwitches = true;
+bool showPlugs = true;
+bool showAdcValues = false;
+bool showPinValues = false;
+bool showBrightness = false;
+bool scanI2C = false;
+bool blink = false;
+int delayMs = 100;
 
 void setup() {
   Serial.begin(115200);
+  Serial.setTxTimeoutMs(0);
   Serial.println("Hello, world!");
+
+  pinMode(BLINK_LED_PIN, OUTPUT);
+  digitalWrite(BLINK_LED_PIN, HIGH);
 
   I2C::init();
   const auto errors = ADC::init();
@@ -42,7 +91,12 @@ void setup() {
     setupErrors.push_back("Failed to initialize MCP23017");
   }
 
+  if (!pixels.begin()) {
+    setupErrors.push_back("Failed to initialize NeoPixel");
+  }
+
   if (!setupErrors.empty()) {
+    blink = true;
     Serial.print("Errors during setup.. ");
     for (int i = 9; i > 0; i--) {
       Serial.print(i);
@@ -54,17 +108,16 @@ void setup() {
   }
 }
 
-bool showPotis = true;
-bool showButtons = true;
-bool showSwitches = true;
-bool showAdcValues = false;
-bool showPinValues = false;
-bool scanI2C = false;
-int delayMs = 100;
-
+bool blinkOn = false;
 int loopCount = 0;
 void loop() {
   loopCount++;
+
+  if (blink) {
+    digitalWrite(BLINK_LED_PIN, blinkOn ? HIGH : LOW);
+    blinkOn = !blinkOn;
+  }
+
   handleSerial();
   Data data;
   const auto adcResults = ADC::readAll();
@@ -75,6 +128,8 @@ void loop() {
   }
   data.adcValues = std::get<1>(adcResults);
   data.pins = mcp.readGPIOAB();
+
+  float brightness = std::clamp(data.adcValues[POTI_CONFIG_BRIGHTNESS] * data.adcValues[POTI_BRIGHTNESS], MIN_BRIGHTNESS, MAX_BRIGHTNESS);
 
   Serial.print(loopCount);
   Serial.print(": ");
@@ -104,6 +159,13 @@ void loop() {
     Serial.print(" ");
   }
 
+  if (showPlugs) {
+    for (const auto index : PLUGSS_MAP) {
+      Serial.print(data.pin(index) ? "◎" : "○");
+      Serial.print(" ");
+    }
+  }
+
   if (showAdcValues) {
     for (const auto& val : data.adcValues) {
       Serial.print(val, 3);
@@ -116,6 +178,10 @@ void loop() {
       Serial.print(data.pin(pin) ? "0" : "1");
     }
     Serial.print(" ");
+  }
+
+  if (showBrightness) {
+    Serial.print(String(brightness, 5));
   }
 
   if (scanI2C) {
@@ -134,6 +200,25 @@ void loop() {
   }
 
   Serial.println();
+
+  pixels.clear();
+  constexpr Color off = BLACK;
+  for (uint8_t button = 0; button < BUTTONS_MAP.size(); button++) {
+    pixels.setPixelColor(button, data.pin(BUTTONS_MAP[button]) ? WHITE * brightness : off);
+  }
+  for (uint8_t swtch = 0; swtch < SWITCHES2_MAP.size(); swtch++) {
+    pixels.setPixelColor(swtch + 5, data.pin(SWITCHES2_MAP[swtch]) ? WHITE * brightness : off);
+  }
+  pixels.setPixelColor(11, data.switch3() > 0
+    ? RED * brightness
+    : (data.switch3() < 0
+      ? GREEN * brightness
+      : off));
+  pixels.setPixelColor(12, data.plug(0) ? BLUE * brightness : off);
+  pixels.setPixelColor(13, data.plug(1) ? RED * brightness : off);
+  pixels.setPixelColor(14, data.plug(2) ? YELLOW * brightness : off);
+  pixels.show();
+
   if (delayMs > 0)
     delay(delayMs);
 }
@@ -151,11 +236,17 @@ void handleSerial() {
       case 's':
         showSwitches = !showSwitches;
         break;
+      case 'o':
+        showPlugs = !showPlugs;
+        break;
       case 'a':
         showAdcValues = !showAdcValues;
         break;
       case 'd':
         showPinValues = !showPinValues;
+        break;
+      case 'h':
+        showBrightness = !showBrightness;
         break;
       case 'i':
         scanI2C = !scanI2C;
