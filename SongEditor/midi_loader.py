ebbb034ab6a66f4909360af8264e951028fbd332
Thomas <git@thomas.klierlinge.de>
2025-10-27 11:31:37 +0100

    Fix timing

--- SongEditor/midi_loader.py
+++ SongEditor/midi_loader.py
@@ -13,50 +13,77 @@ class MidiLoader:
         tempo_map = MidiLoader.get_tempo_map(file)
         ticks_per_beat = file.ticks_per_beat
         song = Song(name=file_name, tracks=[])
+
         for midi_track in file.tracks:
-            abs_time = 0
-            last_time = 0
+            abs_ticks = 0
             tempo_index = 0
-            tempo = tempo_map[tempo_index][1]
             on_notes = {}
             notes = []
             track = Track(name=midi_track.name, notes=[], song=song)
+
             for msg in midi_track:
-                time = msg.time * tempo // ticks_per_beat  # convert to µs
-                abs_time += time
+                abs_ticks += msg.time
+
+                while (tempo_index + 1 < len(tempo_map) and
+                       abs_ticks >= tempo_map[tempo_index + 1][0]):
+                    tempo_index += 1
+
+                abs_time_us = MidiLoader.ticks_to_microseconds(
+                    abs_ticks, tempo_map, ticks_per_beat)
+
                 if msg.type == 'note_on' or msg.type == 'note_off':
                     off = msg.velocity == 0 or msg.type == 'note_off'
-                    t = abs_time - last_time
                     if not off:
-                        on_notes[msg.note] = (abs_time, msg.velocity)
+                        on_notes[msg.note] = (abs_time_us, msg.velocity)
                     else:
                         if msg.note in on_notes:
-                            start_tick, velocity = on_notes.pop(msg.note)
-                            duration = abs_time - start_tick
-                            note = Note(start_us=start_tick, duration_us=duration, pitch=msg.note, velocity=velocity, buzzer=Buzzer.NONE, track=track)
+                            start_us, velocity = on_notes.pop(msg.note)
+                            duration = abs_time_us - start_us
+                            note = Note(start_us=start_us, duration_us=duration,
+                                      pitch=msg.note, velocity=velocity,
+                                      buzzer=Buzzer.NONE, track=track)
                             notes.append(note)
 
-                last_time = abs_time
-                while (tempo_index + 1 < len(tempo_map) and abs_time >= tempo_map[tempo_index + 1][0]):
-                    tempo_index += 1
-                tempo = tempo_map[tempo_index][1]
             if notes:
                 track.notes = notes
                 tracks.append(track)
+
         song.tracks = tracks
         return song
 
     @staticmethod
     def get_tempo_map(file: MidiFile) -> list[tuple[int, int]]:
         tempo_map = []
-        abs_time = 0
-        tempo = 500000  # default tempo in µs per beat
-        track = file.tracks[0]  # usually tempo changes are in the first track
-        tempo_map.append((abs_time, tempo))
+        abs_ticks = 0
+        tempo = 500000
+        track = file.tracks[0]
+        tempo_map.append((abs_ticks, tempo))
         for msg in track:
-            time = msg.time * tempo // file.ticks_per_beat  # convert to µs
-            abs_time += time
+            abs_ticks += msg.time
             if msg.type == 'set_tempo':
                 tempo = msg.tempo
-                tempo_map.append((abs_time, tempo))
+                tempo_map.append((abs_ticks, tempo))
         return tempo_map
+
+    @staticmethod
+    def ticks_to_microseconds(ticks: int, tempo_map: list[tuple[int, int]],
+                             ticks_per_beat: int) -> int:
+        us = 0
+        last_tick = 0
+        tempo = tempo_map[0][1]
+
+        for tempo_tick, new_tempo in tempo_map:
+            if tempo_tick >= ticks:
+                remaining_ticks = ticks - last_tick
+                us += remaining_ticks * tempo // ticks_per_beat
+                break
+            else:
+                tick_delta = tempo_tick - last_tick
+                us += tick_delta * tempo // ticks_per_beat
+                last_tick = tempo_tick
+                tempo = new_tempo
+        else:
+            remaining_ticks = ticks - last_tick
+            us += remaining_ticks * tempo // ticks_per_beat
+
+        return us
